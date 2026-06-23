77adbc40527bedfa6cd9655d81d7f9bcd768341d
Antonio Cleison <a.cleison@live.com>
2026-06-23 17:25:40 -0300

    Commit inicial

---
+++ src/main/java/io/github/aprovadespesas/AprovadespesasApplication.java
@@ -0,0 +1,13 @@
+package io.github.aprovadespesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprovadespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprovadespesasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/aprovadespesas/AprovadespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aprovadespesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprovadespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
