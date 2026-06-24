9309a52c1fa799411cae332aae6f003dd5641955
Antonio Cleison <a.cleisonn@gmail.com>
2026-06-24 15:07:30 -0300

    Implemetna dto request para Login e Register de um User

--- src/main/java/io/github/aprovadespesas/dto/request/LoginRequest.java
+++ src/main/java/io/github/aprovadespesas/dto/request/LoginRequest.java
@@ -1,4 +1,18 @@
 package io.github.aprovadespesas.dto.request;
 
-public record LoginRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+public record LoginRequest(
+        @Email(message = "Email deve ser um email valido.")
+        @NotBlank(message = "O Email é obrigatório.")
+        @Size(max = 150, message = "Email deve ter no máximo 150 caracteres.")
+        String email,
+
+        @NotBlank(message = "Senha é obrigatória")
+        @Size(min = 6, max = 16, message = "Senha deve ter entre 6 a 20 caracteres.")
+        String password
+) {
 }
--- src/main/java/io/github/aprovadespesas/dto/request/RegisterUserRequest.java
+++ src/main/java/io/github/aprovadespesas/dto/request/RegisterUserRequest.java
@@ -1,4 +1,34 @@
 package io.github.aprovadespesas.dto.request;
 
-public record RegisterUserRequest() {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.github.aprovadespesas.entity.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record RegisterUserRequest(
+
+        @NotBlank(message = "O nome é obrigatório.")
+        @Size(max = 130, message = "Nome deve ter no máximo 130 caracteres.")
+        String name,
+
+        @NotBlank(message = "O Email é obrigatório.")
+        @Email(message = "O Email deve ser um email valido.")
+        @Size(max = 150, message = "Email deve ter no máximo 150 caracteres.")
+        String email,
+
+        @NotBlank(message = "A Senha é obrigatória.")
+        @Size(min = 6, max = 16, message = "A Senha deve ter entre 6 a 16 caracteres.")
+        String password,
+
+        @NotNull
+        Role role,
+
+        Long departmentId
+
+) {
 }
