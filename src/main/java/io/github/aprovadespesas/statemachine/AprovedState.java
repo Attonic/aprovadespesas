6a445945c45864b7088f9bd078b9010a531a6dea
Antonio Cleison <a.cleisonn@gmail.com>
2026-07-29 18:45:25 -0300

    implementa classe de status para aprovados

--- src/main/java/io/github/aprovadespesas/statemachine/AprovedState.java
+++ src/main/java/io/github/aprovadespesas/statemachine/AprovedState.java
@@ -1,4 +1,31 @@
 package io.github.aprovadespesas.statemachine;
 
-public class AprovedState {
+import io.github.aprovadespesas.entity.Expense;
+import io.github.aprovadespesas.entity.User;
+
+import java.time.LocalDateTime;
+
+public class AprovedState implements ExpenseState{
+
+    //TODO corrigir essa exception para uma personalizada = InvelidStateTransitionException
+    @Override
+    public void approve(Expense expense, User review) {
+        throw new RuntimeException("Despesa já está aprovada");
+    }
+
+    @Override
+    public void reject(Expense expense, User reviewer, String reason) {
+        throw new RuntimeException("Despesa aprovada não pode ser rejeitada.");
+    }
+
+    @Override
+    public void pay(Expense expense, User review) {
+        expense.setReviewer(review);
+        expense.setPaidAt(LocalDateTime.now());
+    }
+
+    @Override
+    public void cancel(Expense expense, User review) {
+
+    }
 }
