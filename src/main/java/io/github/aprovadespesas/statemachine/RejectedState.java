a39b19e31dde91c4341073e315f2c421c0eccf84
Antonio Cleison <a.cleisonn@gmail.com>
2026-07-30 14:06:03 -0300

    implementa demais status para depesas

---
+++ src/main/java/io/github/aprovadespesas/statemachine/RejectedState.java
@@ -0,0 +1,26 @@
+package io.github.aprovadespesas.statemachine;
+
+import io.github.aprovadespesas.entity.Expense;
+import io.github.aprovadespesas.entity.User;
+
+public class RejectedState implements ExpenseState{
+    @Override
+    public void approve(Expense expense, User review) {
+        //TODO implementar InvalidStateTransitionException
+    }
+
+    @Override
+    public void reject(Expense expense, User reviewer, String reason) {
+        //TODO implementar InvalidStateTransitionException
+    }
+
+    @Override
+    public void pay(Expense expense, User review) {
+        //TODO implementar InvalidStateTransitionException
+    }
+
+    @Override
+    public void cancel(Expense expense, User review) {
+        //TODO implementar InvalidStateTransitionException
+    }
+}
