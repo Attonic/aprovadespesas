63f80597456f38412488899c4db3ebcc6cb014e6
Antonio Cleison <a.cleisonn@gmail.com>
2026-07-31 00:06:39 -0300

    implementa classe de configuração de segurnça JWT

--- src/main/java/io/github/aprovadespesas/security/SecurityConfig.java
+++ src/main/java/io/github/aprovadespesas/security/SecurityConfig.java
@@ -1,4 +1,58 @@
 package io.github.aprovadespesas.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    private static final String[] PUBLIC_ROUTES = {
+            "/api/auth/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/v3/api-docs/**",
+            "/api-docs/**"
+    };
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+       return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        s ->
+                                s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PUBLIC_ROUTES).permitAll()
+                        .requestMatchers("/admin/**").hasAllAuthorities("ROLE_ADMIN")
+                        .anyRequest().authenticated()
+                ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
