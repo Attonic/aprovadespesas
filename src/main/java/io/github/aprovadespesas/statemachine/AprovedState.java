bff993a7a2306fc8d6d39a95d1e84bc0a908e71f
Antonio Cleison <a.cleisonn@gmail.com>
2026-08-12 09:03:12 -0300

    Correção de exceção personalizada em PedingState

--- src/main/java/io/github/aprovadespesas/statemachine/AprovedState.java
+++ src/main/java/io/github/aprovadespesas/statemachine/AprovedState.java
@@ -1,21 +1,23 @@
 package io.github.aprovadespesas.statemachine;
 
+import com.sun.jdi.request.InvalidRequestStateException;
 import io.github.aprovadespesas.entity.Expense;
 import io.github.aprovadespesas.entity.User;
+import io.github.aprovadespesas.exception.InvalidStateTransitionException;
 
 import java.time.LocalDateTime;
 
 public class AprovedState implements ExpenseState{
 
-    //TODO corrigir essa exception para uma personalizada = InvelidStateTransitionException
+
     @Override
     public void approve(Expense expense, User review) {
-        throw new RuntimeException("Despesa já está aprovada");
+        throw new InvalidStateTransitionException("Despesa já está aprovada");
     }
 
     @Override
     public void reject(Expense expense, User reviewer, String reason) {
-        throw new RuntimeException("Despesa aprovada não pode ser rejeitada.");
+        throw new InvalidStateTransitionException("Despesa aprovada não pode ser rejeitada.");
     }
 
     @Override
--- src/main/java/io/github/aprovadespesas/statemachine/CancelledState.java
+++ src/main/java/io/github/aprovadespesas/statemachine/CancelledState.java
@@ -2,26 +2,27 @@ package io.github.aprovadespesas.statemachine;
 
 import io.github.aprovadespesas.entity.Expense;
 import io.github.aprovadespesas.entity.User;
+import io.github.aprovadespesas.exception.InvalidStateTransitionException;
 
 public class CancelledState implements ExpenseState{
 
     @Override
     public void approve(Expense expense, User review) {
-        //TODO implementar InvalidStateTransitionException
+        throw  new InvalidStateTransitionException("Despesa cancelada não pode ser aprovada.");
     }
 
     @Override
     public void reject(Expense expense, User reviewer, String reason) {
-        //TODO implementar InvalidStateTransitionException
+        throw new InvalidStateTransitionException("Despesa cancelada não pode ser rejeitada.");
     }
 
     @Override
     public void pay(Expense expense, User review) {
-        //TODO implementar InvalidStateTransitionException
+        throw new InvalidStateTransitionException("Despesa cancelada não pode ser paga.");
     }
 
     @Override
     public void cancel(Expense expense, User review) {
-        //TODO implementar InvalidStateTransitionException
+        throw new InvalidStateTransitionException("Despesa cancelada não pode ser cancelada.");
     }
 }
